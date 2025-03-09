ca8fb86deef7451df33222152f5854568c247bd4
RoshakaDD <roshakadinusara@gmail.com>
2025-03-10 01:25:51 +0530

    feat: Add HomeScreen with basic UI layout

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Hide debug banner
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(), // HomeScreen is properly defined as a widget
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: const Center(child: Text('Welcome to Home Screen')),
+    );
+  }
+}
