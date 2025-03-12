2c0062130af861a79f2a464f53bf69c198b878e8
Thenuja <thenujajperera@gmail.com>
2025-03-12 19:34:57 +0530

    intial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Validate from '../middleware/validate.js';
+import { registerHiker, registerResponder } from '../controllers/register.js';
+import { Login } from '../controllers/login.js';
+import { check } from 'express-validator';
+import { verifyToken } from "../middleware/verify.js";
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to Trailblaze API v1!', status: 'success' });
+});
+
+router.post("/register/hiker", Validate, registerHiker);
+router.post("/register/responder", Validate, registerResponder);
+router.post("/login", check("email").isEmail().normalizeEmail(), check("password").not().isEmpty(), Validate, Login);
+router.get("/verified", verifyToken, (req, res) => res.status(200).json({ message: "Welcome to Trailblaze!" }));
+
+export default (server) => server.use('/api/v1', router);
