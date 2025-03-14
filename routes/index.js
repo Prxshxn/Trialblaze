edc7fa1df14c18d0238260f983f4ee54795d67a4
Thenuja <thenujajperera@gmail.com>
2025-03-14 11:31:14 +0530

    added logout api

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import { registerHiker, registerResponder } from '../controllers/register.js';
 import { Login } from '../controllers/login.js';
 import { check } from 'express-validator';
 import { verifyToken } from "../middleware/verify.js";
+import { Logout } from '../controllers/logout.js';
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.post("/register/hiker", Validate, registerHiker);
 router.post("/register/responder", Validate, registerResponder);
 router.post("/login", check("email").isEmail().normalizeEmail(), check("password").not().isEmpty(), Validate, Login);
 router.get("/verified", verifyToken, (req, res) => res.status(200).json({ message: "Welcome to Trailblaze!" }));
+router.post("/logout", Logout);
 
 export default (server) => server.use('/api/v1', router);
