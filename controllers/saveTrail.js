ef573546360cd3c8314fdd982efbb15396714b67
Thenuja <thenujajperera@gmail.com>
2025-03-17 07:54:00 +0530

    saves users input when saving trails

--- controllers/saveTrail.js
+++ controllers/saveTrail.js
@@ -2,7 +2,7 @@ import supabase from "../config/supabaseClient.js";
 
 
 export const saveTrail = async (req, res) => {
-  const { name, description, distance, duration,user_id, coordinates } = req.body;
+  const { name, description, distance, duration,user_id, coordinates, district, difficulty_level, elevation_gain } = req.body;
   console.log('Request body:', req.body);
   try {
     // Save trail data
@@ -14,6 +14,9 @@ export const saveTrail = async (req, res) => {
         distance_meters: distance,
         duration_seconds: duration,
         user_id,
+        district, // Add district
+        difficulty_level, // Add difficulty level
+        elevation_gain,
       })
       .select('id')
       .single();
