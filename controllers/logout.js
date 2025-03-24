484625120f5c535207954787b26029a6476f5f14
Dine003 <141012602+Dine003@users.noreply.github.com>
2025-03-24 10:58:42 +0530

    Updated the Logout API

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,9 +1,18 @@
+/**
+ * Logout API endpoint
+ * This function handles the logout process by clearing the session cookie and returning a success response.
+ * 
+ * @param{Object} req - The request object containing client request data.
+ * @param{Object} res - The response object used to send data back to the client.
+ * @returns {Object} - Returns a JSON response indicating the status of the logout operation.
+ */
 export async function Logout(req, res) {
     try {
+        // Clear the session cookie named "SessionID"
         res.clearCookie("SessionID", {
-            httpOnly: true,
-            secure: true,
-            sameSite: "None",
+            httpOnly: true,  // Prevents client-side scripts from accessing the cookie
+            secure: true,   // Ensures the cookie is only sent over HTTPS
+            sameSite: "None",    // Allows the cookie to be sent with cross-site requests
         });
 
         return res.status(200).json({
